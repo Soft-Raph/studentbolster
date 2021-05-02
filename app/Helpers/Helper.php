6c2819a1ce38b4b1a6a5da230669143416423ec4
Adams Paul <adamsohiani@gmail.com>
2021-05-02 01:39:47 +0100

    Fixes

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -27,7 +27,7 @@ class Helper
 
        $trans = auth()->user()->transactions->where('type', 'Investment')->last();
         if (is_null($trans)){
-            return false;
+            return true;
         }
        $start_date = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s',$trans->created_at);
        $end_date = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', date("Y-m-d H:i:s"));
